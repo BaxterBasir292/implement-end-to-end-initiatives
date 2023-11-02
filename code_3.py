99ef086f6a4deb45c6677489b205c631301360c8
Basir <140265607+BaxterBasir292@users.noreply.github.com>
2023-11-02 05:44:13 +0100

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,14 +1,59 @@
+
+
+function generateRandomData() {
+}
+    }
+    });
+        console.log("Random Number: " + item);
+    const data = [];
+    for (let i = 0; i < 10; i++) {
+    const data = generateRandomData();
+    return data;
+        data.push(Math.floor(Math.random() * 100) + 1);
+    data.forEach(item => {
+
+main();
+}
+function main() {    return data
     data = [random.randint(1, 100) for _ in range(10)]
-def generate_random_data():
-import random
+
+
+
+    data = generate_random_data()
+if __name__ == "__main__":
     main()
 
-
+def main():
+import random
+        print(f"Random Number: {item}")
     for item in data:
-    data = generate_random_data()
+def generate_random_data():def main():
+    main()
+    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+
+import random
 
     return data
-def main():
         print(f"Random Number: {item}")
 
 if __name__ == "__main__":
+def generate_random_data():
+
+    data = generate_random_data()main();
+    data.forEach(item => {
+    const data = generateRandomData();
+function main() {
+    const data = [];
+
+        console.log("Random Number: " + item);
+    for (let i = 0; i < 10; i++) {
+
+    return data;
+function generateRandomData() {
+    });
+}
+}
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+    }
