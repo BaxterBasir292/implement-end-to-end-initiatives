abcb54a3c469362581f1a8aa0dc91977ae2e66ae
Basir <140265607+BaxterBasir292@users.noreply.github.com>
2023-11-03 17:46:00 +0100

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,40 +1,42 @@
-    for item in data:
-
-def generate_random_data():
-if __name__ == "__main__":
-    data = generate_random_data()
         print(f"Random Number: {item}")
-    data = [random.randint(1, 100) for _ in range(10)]
 
-    return data
+if __name__ == "__main__":
 import random
-
-def main():
-    main()
+    for item in data:
     data = generate_random_data()
 
-def generate_random_data():
-    return data
-
-
-import random
-    main()
 
     data = [random.randint(1, 100) for _ in range(10)]
-if __name__ == "__main__":
+    main()
+def generate_random_data():
+
 def main():
-    for item in data:
-        print(f"Random Number: {item}")
+    return data    for item in data:
+
     data = generate_random_data()
+
 def generate_random_data():
 
+    return data
 
 import random
-    data = [random.randint(1, 100) for _ in range(10)]
-def main():
-
         print(f"Random Number: {item}")
-if __name__ == "__main__":
-    return data
-    for item in data:
     main()
+def main():
+    data = [random.randint(1, 100) for _ in range(10)]
+if __name__ == "__main__":    });
+function generateRandomData() {
+    }
+    const data = [];
+    const data = generateRandomData();
+}
+
+        console.log("Random Number: " + item);
+    return data;
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+    data.forEach(item => {
+}
+    for (let i = 0; i < 10; i++) {
+function main() {
+main();
